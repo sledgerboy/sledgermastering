c2052f486c31093c22ca8d6d7b7ace574bdde1eb
Nekrasov Dmitriy <nekrasovdy@belhard.com>
2023-01-12 12:11:37 +0300

    v3

---
+++ !bckp/footer.php
@@ -0,0 +1,157 @@
+   <!-- Footer
+   ================================================== -->
+   <footer>
+
+      <div class="row">  
+
+      	<div class="twelve columns content group">
+      		
+				<ul class="social-links">
+               <li><a target="_blank" rel="nofollow" href="https://fb.com/sledgermastering"><i class="fa fa-facebook-square"></i></a></li>
+               <li><a target="_blank" rel="nofollow" href="https://www.youtube.com/@sledgermastering2015"><i class="fa fa-youtube-play"></i></a></li>
+               <li><a target="_blank" rel="nofollow" href="https://www.soundcloud.com/sledgermastering"><i class="fa fa-soundcloud"></i></a></li>
+               <li><a target="_blank" rel="nofollow" href="https://www.instagram.com/sledger.music"><i class="fa fa-instagram"></i></a></li>
+               <li><a rel="nofollow" href="skype:sled-ger?chat"><i class="fa fa-skype"></i></a></li>
+               <li><a rel="nofollow" href="viber://chat/?number=+375291129746"><i class="fa fa-viber"></i></a></li>
+    			<!-- <li><a rel="nofollow" target="_blank" href="https://soundbetter.com/profiles/36407-sledger-music-mastering" title="Sledger profile on SoundsBetter"><img src="img/icons/sb.png" width="32" alt="Sledger Mastering profile on SoundsBetter"></a></li-->
+            </ul>
+
+            <hr />
+
+            <div id="go-top" class="styled_button">
+               <a class="smoothscroll styled_button" title="Back to Top" href="#hero">Back to Top<i class="fa fa-angle-up"></i></a>
+            </div>
+
+            <!--div class="info">
+
+            	<div class="footer-logo"></div>
+
+	            <p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
+	            Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem
+	            nibh id elit. 
+	            </p>	        
+
+	         </div-->
+
+      	</div>           
+
+         <?php $today = date("Y");?>
+
+
+         <ul class="copyright">
+         	<li>&copy; Copyright <?=$today?></li> 
+            <li><a title="Sledgermastering" href="/">Sledgermastering.by</a>.</li> 
+         	
+         </ul>
+
+
+
+      </div> <!-- end row -->
+
+   </footer> <!-- end footer -->
+
+   <div id="preloader"> 
+    	<div id="loader"></div>
+   </div> 
+
+   <!-- Java Script
+   ================================================== --> 
+   <script src="js/jquery-1.11.3.min.js"></script>
+   <script src="js/jquery-migrate-1.2.1.min.js"></script>
+   <script src="js/jquery.flexslider-min.js"></script>
+   <script src="js/jquery.waypoints.min.js"></script>
+   <script src="js/jquery.validate.min.js"></script>
+   <script src="js/jquery.fittext.js"></script>
+   <script src="js/jquery.placeholder.min.js"></script>
+   <script src="js/jquery.magnific-popup.min.js"></script>  
+   <script src="js/main.js"></script>
+   <script src="js/pricer.js"></script>
+   <script>
+   /*---------------------------------------------------- */
+	/* Prices tabs
+	------------------------------------------------------ */ 
+		function openPrice(evt, priceName) {
+			var i, tabcontent, tablinks;
+			tabcontent = document.getElementsByClassName("tabcontent");
+			for (i = 0; i < tabcontent.length; i++) {
+				tabcontent[i].style.display = "none";
+			}
+			tablinks = document.getElementsByClassName("tablinks");
+			for (i = 0; i < tablinks.length; i++) {
+				tablinks[i].className = tablinks[i].className.replace(" active", "");
+			}
+			document.getElementById(priceName).style.display = "block";
+			evt.currentTarget.className += " active";
+		}
+   </script>
+   <script>
+   /*---------------------------------------------------- */
+	/* Prices Currencies tabs
+	------------------------------------------------------ */ 
+		function openPriceCur(evt, curDivName) {
+         
+			var i, tabcontent_cur, tablinks;
+			tabcontent_cur = document.getElementsByClassName("tabcontent_cur");
+			for (i = 0; i < tabcontent_cur.length; i++) {
+				tabcontent_cur[i].style.display = "none";
+			}
+			tablinks = document.getElementsByClassName("tablinks");
+			for (i = 0; i < tablinks.length; i++) {
+				tablinks[i].className = tablinks[i].className.replace(" active", "");
+			}
+			document.getElementById(curDivName).style.display = "block";
+			evt.currentTarget.className += " active";
+		}
+   </script>
+   
+   <script>
+      $('#for_labels > div.tab > a').click(function(){
+         $("#flb").addClass('active');
+      })
+   </script>
+
+   <script>
+      $('#flb').click(function(){
+         $("#for_labels > div.tab > a:nth-child(1)").addClass('active');
+      })
+   </script>
+
+   <script>
+   /*---------------------------------------------------- */
+	/* Prices Artists Currencies tabs
+	------------------------------------------------------ */ 
+		function openPriceArtCur(evt, curDivArtistsName) {
+         
+			var i, tabcontent_artcur, tablinks;
+			tabcontent_artcur = document.getElementsByClassName("tabcontent_artcur");
+			for (i = 0; i < tabcontent_artcur.length; i++) {
+				tabcontent_artcur[i].style.display = "none";
+			}
+			tablinks = document.getElementsByClassName("tablinks");
+			for (i = 0; i < tablinks.length; i++) {
+				tablinks[i].className = tablinks[i].className.replace(" active", "");
+			}
+			document.getElementById(curDivArtistsName).style.display = "block";
+			evt.currentTarget.className += " active";
+		}
+   </script>
+   
+   <script>
+   $('#for_artists > div.tab > a').click(function(){
+      $("#fab").addClass('active');
+   })
+   </script>
+
+   <script>
+      $("#fab").click(function(){
+         $("#for_artists > div.tab > a:nth-child(1)").addClass('active');
+      })
+   </script>
+
+
+
+
+
+
+
+
---
+++ !bckp/head.php
@@ -0,0 +1,38 @@
+<?php
+?>
+<!DOCTYPE html>
+<!--[if IE 8 ]><html class="no-js oldie ie8" lang="en"> <![endif]-->
+<!--[if IE 9 ]><html class="no-js oldie ie9" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<title>Sledger Mastering - Mastering service for labels and distributors</title>
+	<meta name="description" content="Online mastering service for labels, distributors and artists. Flexible price systems. Discounts and free demo!">  
+	<meta name="author" content="sledger">
+	<meta name="robots" content="index, follow">
+	<meta name="googlebot" content="index, follow">
+	<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+	<meta http-equiv="Pragma" content="no-cache">
+	<meta http-equiv="Expires" content="12600">
+
+	<!-- mobile specific metas
+	================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+	================================================== -->
+	<link rel="stylesheet" href="css/base.css">
+	<link rel="stylesheet" href="css/vendor.min.css">
+	<link rel="stylesheet" href="css/main.css?v=45722">     
+	<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;700&display=swap" rel="stylesheet">
+
+	<!-- script
+	================================================== -->
+	<script src="js/modernizr.js"></script>
+
+	<!-- favicons
+	================================================== -->
+	<link rel="shortcut icon" href="favicon.png" >
+
+	<style> @import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;700&display=swap'); </style>
+</head>
